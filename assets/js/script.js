5bd3bb897b40e54541083c4ee23c401284f56060
NUr Muhamad Soleh <nursoleh33@gmail.com>
2021-02-20 10:22:41 +0700

    merge conflik

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 const bootcamp = "Full-stack Javascript";
-const nama = "NUR MUHAMAD SOLEH";
-=======
 const bootcamp = "Full-stack Javascript"
+const nama = "NUR MUHAMAD SOLEH";
 const nama = "Muhammad Kifahi Al Tsaqofy"
->>>>>>> b5c2e67bd91e664abe7c8cfad3511e749fcf22ba
